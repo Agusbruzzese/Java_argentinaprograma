e9a77f53b04d73f47d3a71740468fe6e19dfd90b
Agustin <Agustin.salvador.bruzzese@gmail.com>
2023-08-09 14:34:52 -0300

    ejercicio2 completo

---
+++ ej2/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("ingrese un numero: ");
+        int num = scanner.nextInt();
+
+        System.out.printf("El numero ingresado es: "+ num);
+    }
+}
